d4e1183c5d227a2039a8ebf791cfa95d523dc6b8
Geoffrey Wong <gwonghk@gmail.com>
2023-07-22 20:50:47 -0400

    init eslint typescript

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -9,11 +9,12 @@ const Footer = () => {
     <footer className={styles.footer}>
       <Wrapper>
         <Text color="accents-7">
-          Made with ❤️, 🔥, and a keyboard by{' '}
+          Footer
+          {/* Made with ❤️, 🔥, and a keyboard by{' '}
           <TextLink href="https://hoangvvo.com/" color="link">
             Hoang Vo
           </TextLink>
-          .
+          . */}
         </Text>
         <Spacer size={1} axis="vertical" />
         <ThemeSwitcher />
